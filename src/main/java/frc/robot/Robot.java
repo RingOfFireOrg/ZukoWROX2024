300980db92804e264b1e877453a18e116733daa2
Anoop Kodadhala <73144205+AnoopKodadhala@users.noreply.github.com>
2024-07-26 23:45:31 -0400

    Reformatted Files, Fixed some erros

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -8,6 +8,7 @@ import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import frc.robot.subsystems.Drivetrainsubsystem;
+import frc.robot.subsystems.Shootersubsystem;
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
@@ -21,6 +22,8 @@ public class Robot extends TimedRobot {
   private RobotContainer m_robotContainer;
 
     public static Drivetrainsubsystem myDriveTrain = new Drivetrainsubsystem();
+    public static Shootersubsystem shootersubsystem = new Shootersubsystem();
+
 
 
   /**
